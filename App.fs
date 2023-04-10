00cc61b1673fb6668a866b5f883d08f680e727df
nojaf <florian.verdonck@outlook.com>
2023-04-10 21:25:18 +0200

    Initial commit.

---
+++ App.fs
@@ -0,0 +1,102 @@
+﻿module App
+
+open System
+open Fable.Core
+open Fable.Core.JsInterop
+open Browser.Dom
+open Fable.React
+open Fable.React.Props
+open Feliz
+
+#if DEBUG
+importSideEffects "./WebSocket.js"
+#endif
+
+type React =
+    static member useState<'T>(_initialState: 'T) : 'T * ('T -> unit) = import "useState" "react"
+    static member useEffect(_action: unit -> unit, _dependencies: obj array) : unit = import "useEffect" "react"
+
+let useCopyToClipboard () : string * (string -> unit) =
+    import "useCopyToClipboard" "usehooks-ts"
+
+// https://colorsea.js.org/pages/QuickStart.html#import
+[<AbstractClass>]
+type ColourSea =
+    [<Emit("$0.darken($1)")>]
+    abstract member darken: amount: int -> ColourSea
+
+    [<Emit("$0.hex($1)")>]
+    abstract member hex: unit -> string
+
+let colourSea (_: string) : ColourSea = importDefault "colorsea"
+
+let isBlackTextOk (hex: string) =
+    if String.IsNullOrWhiteSpace hex || hex.Length < 7 then
+        false
+    else
+        let color = if hex.[0] = '#' then hex.Substring(1, 6) else hex
+        let hexToFloat v = Convert.ToInt32(v, 16) |> float
+        let r = hexToFloat (color.Substring(0, 2))
+        let g = hexToFloat (color.Substring(2, 2))
+        let b = hexToFloat (color.Substring(4, 2))
+        (r * 0.299) + (g * 0.587) + (b * 0.114) > 186.0
+
+[<ReactComponent>]
+let App () =
+    let colour, setColour = React.useState "#42A5E7"
+    let name, setName = React.useState "primary"
+    let copyButtonText, setCopyButtonText = React.useState "Click to copy"
+    let _, copy = useCopyToClipboard ()
+
+    React.useEffect (
+        fun () ->
+            if copyButtonText = "Copied!" then
+                JS.setTimeout (fun () -> setCopyButtonText "Click to copy") 400 |> ignore
+        , [| copyButtonText |]
+    )
+
+    let result name colour =
+        let hoverColour = colourSea(colour).darken(5).hex ()
+        let borderColour = colourSea(colour).darken(15).hex ()
+        let textColour = if isBlackTextOk colour then "black" else "white"
+
+        let resultCode =
+            $"""    --%s{name}: %s{colour};
+    --%s{name}-hover: %s{hoverColour};
+    --%s{name}-border: %s{borderColour};"""
+
+        let resultText =
+            $""":root {{
+%s{resultCode}
+}}"""
+
+        div [ Id "results" ] [
+            code [] [ pre [] [ str resultText ] ]
+            button [
+                OnClick(fun ev ->
+                    ev.preventDefault ()
+                    copy resultCode
+                    setCopyButtonText "Copied!")
+                OnMouseEnter(fun ev -> ev?target?style?backgroundColor <- hoverColour)
+                OnMouseLeave(fun ev -> ev?target?style?backgroundColor <- colour)
+                Style [
+                    Color textColour
+                    BackgroundColor colour
+                    Border $"1px solid {borderColour}"
+                    Cursor "pointer"
+                ]
+            ] [ str copyButtonText ]
+        ]
+
+    fragment [] [
+        form [] [
+            input [ Type "color"; Value colour; OnChange(fun e -> setColour e.Value) ]
+            input [ Type "text"; Value colour; OnChange(fun e -> setColour e.Value) ]
+            label [] [ str "variable name" ]
+            input [ Type "text"; DefaultValue name; OnChange(fun e -> setName e.Value) ]
+        ]
+        result name colour
+    ]
+
+let app = document.querySelector "main"
+ReactDom.render (App(), app)
---
+++ dev-server.fsx
@@ -0,0 +1,186 @@
+#r "nuget: Suave, 2.6.2"
+#r "nuget: FSharp.Data.Adaptive, 1.2.13"
+#r "nuget: CliWrap, 3.6.0"
+#r "nuget: Fake.IO.FileSystem, 6.0.0"
+
+open System
+open System.Collections.Concurrent
+open System.IO
+open System.Text
+open System.Net
+open System.Threading
+open System.Threading.Tasks
+open CliWrap
+open CliWrap.EventStream
+open FSharp.Data.Adaptive
+open Suave
+open Suave.Logging
+open Suave.Files
+open Suave.Sockets
+open Suave.Sockets.Control
+open Suave.WebSocket
+open Suave.Filters
+open Suave.Operators
+open Suave.RequestErrors
+open Fake.IO
+open Fake.IO.FileSystemOperators
+open Fake.IO.Globbing.Operators
+
+let exitTask = TaskCompletionSource<unit>()
+let cts = new CancellationTokenSource()
+
+Console.CancelKeyPress.Add(fun _ ->
+    printfn "Goodbye"
+    cts.Cancel()
+    exitTask.SetResult())
+
+let dist = __SOURCE_DIRECTORY__ </> "dist"
+
+let port = 3000us
+
+let build () =
+    Shell.rm_rf dist
+
+    Cli
+        .Wrap("dotnet")
+        .WithArguments("fable -e .js")
+        .WithWorkingDirectory(__SOURCE_DIRECTORY__)
+        .ExecuteAsync()
+        .Task.Wait()
+
+    !!(__SOURCE_DIRECTORY__ </> "**/*.js")
+    |> GlobbingPattern.setBaseDir __SOURCE_DIRECTORY__
+    |> Shell.copyFilesWithSubFolder dist
+
+    [ "index.html"; "style.css"; "favicon.ico" ]
+    |> List.map (fun file -> __SOURCE_DIRECTORY__ </> file)
+    |> Shell.copy dist
+
+let serveFiles =
+    [ GET >=> path "/" >=> file (__SOURCE_DIRECTORY__ </> "index.html")
+      GET >=> browseHome
+      NOT_FOUND "Page not found." ]
+
+let args =
+    fsi.CommandLineArgs
+    |> Array.skipWhile (fun arg -> arg.EndsWith ".fsx")
+    |> Array.map String.toLowerInvariant
+    |> Array.toList
+
+match args with
+| [ "help" ]
+| [ "--help" ] ->
+    printfn "Available commands: build, preview, watch"
+    exit 0
+| [ "build" ] -> build ()
+| [ "preview" ] ->
+    build ()
+
+    let conf =
+        { defaultConfig with
+            cancellationToken = cts.Token
+            homeFolder = Some(__SOURCE_DIRECTORY__ </> "dist")
+            compressedFilesFolder = Some(Path.GetTempPath())
+            bindings = [ HttpBinding.create HTTP IPAddress.Loopback port ] }
+
+    let _, suaveServer = startWebServerAsync conf (choose serveFiles)
+
+    Async.Start(suaveServer, cts.Token)
+    exitTask.Task.Wait()
+| []
+| [ "watch" ] ->
+    let fableHandle =
+        Cli
+            .Wrap("dotnet")
+            .WithArguments("fable watch -e .js --define DEBUG")
+            .WithWorkingDirectory(__SOURCE_DIRECTORY__)
+            .Observe(cancellationToken = cts.Token)
+        |> Observable.subscribe (fun ev ->
+            match ev with
+            | :? StartedCommandEvent as startedEvent -> printfn $"Fable Started: %i{startedEvent.ProcessId}"
+            | :? StandardOutputCommandEvent as stdOutEvent -> printfn $"Fable: %s{stdOutEvent.Text}"
+            | :? ExitedCommandEvent as exitedEvent -> printfn $"Fable Exited: %A{exitedEvent.ExitCode}"
+            | :? StandardErrorCommandEvent as stdErrEvent -> printfn $"Fable ERR: %s{stdErrEvent.Text}"
+            | _ -> ())
+
+    let connectedClients = ConcurrentDictionary<WebSocket, unit>()
+
+    let ws (webSocket: WebSocket) (context: HttpContext) =
+        context.runtime.logger.info (Message.eventX $"New websocket connection")
+        connectedClients.TryAdd(webSocket, ()) |> ignore
+
+        socket {
+            let mutable loop = true
+
+            while loop do
+                let! msg = webSocket.read ()
+
+                match msg with
+                | Text, data, true ->
+                    let str = Encoding.UTF8.GetString data
+                    context.runtime.logger.info (Message.eventX $"Received %s{str} from client")
+                    let response = sprintf "response to %s" str
+                    let byteResponse = response |> Encoding.UTF8.GetBytes |> ByteSegment
+                    do! webSocket.send Text byteResponse true
+
+                | Close, _, _ ->
+                    context.runtime.logger.info (Message.eventX "Closing connection")
+                    connectedClients.TryRemove webSocket |> ignore
+                    let emptyResponse = [||] |> ByteSegment
+                    do! webSocket.send Close emptyResponse true
+                    loop <- false
+
+                | _ -> ()
+        }
+
+    let broadCastReload (msg: string) =
+        let msg = msg |> Encoding.UTF8.GetBytes |> ByteSegment
+
+        connectedClients.Keys
+        |> Seq.map (fun client ->
+            async {
+                let! _ = client.send Text msg true
+                ()
+            })
+        |> Async.Parallel
+        |> Async.Ignore
+        |> Async.RunSynchronously
+
+    let cssWatcher =
+        AdaptiveFile
+            .GetLastWriteTime(Path.Combine(__SOURCE_DIRECTORY__, "style.css"))
+            .AddCallback(fun _ -> broadCastReload "style.css")
+
+    let indexHtmlWatcher =
+        AdaptiveFile
+            .GetLastWriteTime(Path.Combine(__SOURCE_DIRECTORY__, "index.html"))
+            .AddCallback(fun _ -> broadCastReload "full")
+
+    let jsWatcher =
+        AdaptiveDirectory.GetFiles(__SOURCE_DIRECTORY__, pattern = ".*\\.js", recursive = true)
+        |> ASet.map (fun file -> AdaptiveFile.GetLastWriteTime file.FullName)
+        |> ASet.flattenA
+        |> ASet.tryMax
+        |> fun aval -> aval.AddCallback(fun _ -> broadCastReload "full")
+
+    let server = choose [ path "/ws" >=> handShake ws; yield! serveFiles ]
+
+    let conf =
+        { defaultConfig with
+            cancellationToken = cts.Token
+            homeFolder = Some __SOURCE_DIRECTORY__
+            compressedFilesFolder = Some(Path.GetTempPath())
+            bindings = [ HttpBinding.create HTTP IPAddress.Loopback port ] }
+
+    let _, suaveServer = startWebServerAsync conf server
+    Async.Start(suaveServer, cts.Token)
+
+    cts.Token.Register(fun _ ->
+        cssWatcher.Dispose()
+        indexHtmlWatcher.Dispose()
+        jsWatcher.Dispose()
+        fableHandle.Dispose())
+    |> ignore
+
+    exitTask.Task.Wait()
+| args -> failwithf "invalid args: %A" args
